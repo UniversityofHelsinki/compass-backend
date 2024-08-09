e7cbf209c6ceb7b1393ba75fd734a3c0b1e9f7d7
anttilan <antti.lankinen@helsinki.fi>
2024-08-09 12:48:21 +0300

    testing

--- api/routes.js
+++ api/routes.js
@@ -1,16 +1,7 @@
-const { persons  } = require("../models/persons");
 const path = require("path");
 
-/*module.exports = (router) => {
-    router.get('/persons', persons);
-};*/
 module.exports = (router, rootDir) => {
     console.log(`eka rootDir ${rootDir}`);
-    /*router.route('/')
-        .get((req, res) => {
-            console.log(`toka rootDir ${rootDir}`);
-            res.render(path.resolve(rootDir, "./public/index.html"), {nonce: res.locals.cspNonce});
-        });*/
     /*router.route('/').get((req, res) => {
         //res.render(path.resolve('../public/index.html'));
         res.sendFile('index.html', {root : rootDir + '/public'});
