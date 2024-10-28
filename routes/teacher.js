69b36fb608945b0d589d39ab6a93632f3238435e
Vellu <veli-matti.heikkila@helsinki.fi>
2024-10-28 13:50:56 +0200

    add course statistics api endpoint

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,53 +1,58 @@
-
 const { dbClient } = require('./../services/dbService.js');
-const userService = require("../services/userService");
 
 exports.teacher = (router) => {
+    router.get('/courses', async (req, res) => {
+        const user = req.user;
+        res.json(await dbClient(`/api/teacher/courses/${user.eppn}`));
+    });
 
-  router.get('/courses', async (req, res) => {
-    const user = req.user;
-    res.json(await dbClient(`/api/teacher/courses/${user.eppn}`));
-  });
+    router.get('/courses/:course', async (req, res) => {
+        const { course } = req.params;
+        const user = req.user;
+        res.json(await dbClient(`/api/teacher/courses/${user.eppn}/${course}`));
+    });
 
-  router.get('/courses/:course', async (req, res) => {
-    const { course } = req.params;
-    const user = req.user;
-    res.json(await dbClient(`/api/teacher/courses/${user.eppn}/${course}`));
-  });
+    router.post('/courses', async (req, res) => {
+        res.json(
+            await dbClient(`/api/teacher/courses`, {
+                method: 'POST',
+                body: JSON.stringify({ ...req.body, user_name: req.user.eppn }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }),
+        );
+    });
 
-  router.post('/courses', async (req, res) => {
-    res.json(await dbClient(`/api/teacher/courses`, {
-      method: 'POST',
-      body: JSON.stringify({ ...req.body, user_name: req.user.eppn }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }));
-  });
+    router.put('/courses', async (req, res) => {
+        res.json(
+            await dbClient(`/api/teacher/courses`, {
+                method: 'PUT',
+                body: JSON.stringify({ ...req.body, user_name: req.user.eppn }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }),
+        );
+    });
 
-  router.put('/courses', async (req, res) => {
-    res.json(await dbClient(`/api/teacher/courses`, {
-      method: 'PUT',
-      body: JSON.stringify({ ...req.body, user_name: req.user.eppn }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }));
-  });
+    router.get('/courses/:course/questions', async (req, res) => {
+        const course = req.params.course;
+        res.json(await dbClient(`/api/teacher/courses/${course}/questions`));
+    });
 
-  router.get('/courses/:course/questions', async (req, res) => {
-    const course = req.params.course;
-    res.json(await dbClient(`/api/teacher/courses/${course}/questions`));
-  });
+    router.get('/courses/:course/assignments', async (req, res) => {
+        const course = req.params.course;
+        res.json(await dbClient(`/api/teacher/courses/${course}/assignments`));
+    });
 
-  router.get('/courses/:course/assignments', async (req, res) => {
-    const course = req.params.course;
-    res.json(await dbClient(`/api/teacher/courses/${course}/assignments`));
-  });
+    router.get('/courses/:course/students', async (req, res) => {
+        const course = req.params.course;
+        res.json(await dbClient(`/api/teacher/courses/${course}/students`));
+    });
 
-  router.get('/courses/:course/students', async (req, res) => {
-    const course = req.params.course;
-    res.json(await dbClient(`/api/teacher/courses/${course}/students`));
-  });
-
+    router.get('/statistics/course/:course', async (req, res) => {
+        const course = req.params.course;
+        res.json(await dbClient(`/api/teacher/statistics/course/${course}`));
+    });
 };
